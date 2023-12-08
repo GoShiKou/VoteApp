306bf6f35f3428d287caaa8750c223003d993385
NGUYENTHISUONG <suongnguyen182001@gmail.com>
2023-12-08 10:00:41 +0900

    gradleの設定変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "jp.ac.ecc.se.voteapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "jp.ac.ecc.se.voteapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
