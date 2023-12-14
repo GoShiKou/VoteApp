36a359682d842646a32268d41a11df206cf0baf7
NGUYENTHISUONG <suongnguyen182001@gmail.com>
2023-12-14 13:32:06 +0900

    commit

--- app/src/main/java/jp/ac/ecc/se/voteapp/CreatePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/CreatePage.java
@@ -1,16 +1,47 @@
 package jp.ac.ecc.se.voteapp;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import android.provider.MediaStore;
 import android.widget.ListView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageView;
+import android.net.Uri;
+
 public class CreatePage extends AppCompatActivity {
+    final int CAMERA_RESULT = 100;
+    public Uri image;
+    Uri imageUri;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //requestCode == このアプリから読んだカメラの戻り＆＆resultCode == カメラの処理が正常終了
+        if (requestCode == CAMERA_RESULT && resultCode == RESULT_OK) {
+            //カメラからの戻り引数"Data"にある画像データを取り戻す
+//          Bitmap bitmap = data.getParcelableExtra("data");
+            //画面上のパーツImageViewを変数化
+            ImageView cameraImage = findViewById(R.id.image);
+            //受け取った画像データをセット
+            //cameraImage.setImageBitmap(bitmap);
+            cameraImage.setImageURI(imageUri);
+
+        }
+
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,30 +52,41 @@ public class CreatePage extends AppCompatActivity {
         //
         ListView ListView= findViewById(R.id.list);
         EditText choice = findViewById(R.id.editText);
-        Button Button1 = findViewById(R.id.add);
+        Button AddButton = findViewById(R.id.add);
         Button Button2 = findViewById(R.id.button);
-        Button Button3 = findViewById(R.id.camera);
+        Button Camera = findViewById(R.id.camera);
+
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choice.getText().toString().isEmpty()) {
+                    datalist.add(choice.getText().toString());
+                    choice.setText("");
+                }
+            }
+        });
+        Camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //変数名を作成
+                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String fileName = "Traning_" + timestamp + "_.jpg";
+                //ファイル情報のための変数
+                ContentValues values = new ContentValues();
+                //ファイル名をセット
+                values.put(MediaStore.Images.Media.TITLE, fileName);
+                //ファイル形式を設定
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                //保存画像情報の URI を生成する
+                imageUri =
+                        getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                //カメラアプリに送る準備
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                //カメラアプリ起動　戻りあたいあり
+                startActivityForResult(intent,CAMERA_RESULT);
+            }
+        });
+
     }
-
-
-
-
-
-
-
-
-//    private void addNewOption() {
-//        String newOptionText = newOptionEditText.getText().toString().trim();
-//
-//        if (!newOptionText.isEmpty()) {
-//            TextView newOptionTextView = new TextView(this);
-//            newOptionTextView.setText(newOptionText);
-//
-//            // add new option
-//            optionsLayout.addView(newOptionTextView);
-//
-//            // clear input
-//            newOptionEditText.getText().clear();
-//        }
-//    }
 }
