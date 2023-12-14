e6b87d086db98da7ec61091961533e720fa7ef5c
AdiyaDede <gantulga.adiyadelger@gmail.com>
2023-12-14 13:13:11 +0900

    comment layout parts　宣言

--- app/src/main/java/jp/ac/ecc/se/voteapp/Comment.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/Comment.java
@@ -1,14 +1,32 @@
 package jp.ac.ecc.se.voteapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Comment extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        TextView CommentView = findViewById(R.id.CommentView);
+
+
+
+
+
+
+
+
+
     }
 }
