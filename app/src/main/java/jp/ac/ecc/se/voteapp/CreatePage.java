65144279641c88060df90e714f6108f188a21838
NGUYENTHISUONG <suongnguyen182001@gmail.com>
2023-12-14 10:20:59 +0900

    commit

--- app/src/main/java/jp/ac/ecc/se/voteapp/CreatePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/CreatePage.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 public class CreatePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createpage);
+
+        ArrayList<String> datalist = new ArrayList<>();
+        //
+        ListView ListView= findViewById(R.id.list);
+        EditText choice = findViewById(R.id.editText);
+        Button Button1 = findViewById(R.id.add);
+        Button Button2 = findViewById(R.id.button);
     }
 
 
