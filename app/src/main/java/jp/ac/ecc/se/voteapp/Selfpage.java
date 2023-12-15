0c2e5635fb7b32551f11bd3c4e3c0c11ffc0a0bc
MSI\2230336 <chwanchwanya@gmail.com>
2023-12-15 10:31:26 +0900

    Add Layout and Java: selfpage -1215 = Java:selfpage Layout:selfPage prototype done

--- app/src/main/java/jp/ac/ecc/se/voteapp/Selfpage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/Selfpage.java
@@ -1,14 +1,52 @@
 package jp.ac.ecc.se.voteapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Selfpage extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfpage);
+
+        RecyclerView sns = findViewById(R.id.selfVote);
+        ImageView img = findViewById(R.id.selfie);
+        TextView text = findViewById(R.id.selfInfo);
+        Button back = findViewById(R.id.backBtn);
+
+
+
     }
 }
+
+
+//public class Post {
+//    private int avatarResId;
+//    private String username;
+//    private String postContent;
+//
+//    public Post(int avatarResId, String username, String postContent) {
+//        this.avatarResId = avatarResId;
+//        this.username = username;
+//        this.postContent = postContent;
+//    }
+//
+//    public int getAvatarResId() {
+//        return avatarResId;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public String getPostContent() {
+//        return postContent;
+//    }
+//}
+
