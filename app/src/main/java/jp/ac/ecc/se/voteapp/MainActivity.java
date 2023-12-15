fbebd6fe62631546740b1fd9f58b7fa406ab700f
Hiro <travelerhiro5454@gmail.com>
2023-12-15 10:33:43 +0900

    commit

--- app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         Intent create = new Intent(this, CreatePage.class);
 
         Button voteCreate = findViewById(R.id.VoteCreate);
+        ListView voteList = findViewById(R.id.voteList);
+
+        ArrayList<String> dataList = new ArrayList<>();
+        //アダプター
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, dataList);
+        voteList.setAdapter(adapter);
 
         voteCreate.setOnClickListener(new View.OnClickListener() {
             @Override
