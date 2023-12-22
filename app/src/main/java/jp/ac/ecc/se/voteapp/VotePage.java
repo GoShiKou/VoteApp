438cba94e666abfc99e495c83bb3ae7d2c801369
MSI\2230336 <chwanchwanya@gmail.com>
2023-12-22 15:25:20 +0900

    Add Layout and Java: selfpage -1215 = Java:selfpage post adapter(prototype), profile & id adapter Layout:selfPage prototype done

--- app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
@@ -9,11 +9,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class VotePage extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votepage);
+
+
         TextView title= findViewById(R.id.taitoru);
         TextView naiyou = findViewById(R.id.naiyo);
         Button kakutei = findViewById(R.id.kakutei);
