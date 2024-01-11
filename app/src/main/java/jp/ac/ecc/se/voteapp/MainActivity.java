6663bbf2a6a2d644ca7e82f68a47844a84bc01c0
Hiro <travelerhiro5454@gmail.com>
2024-01-11 09:42:35 +0900

    commit

--- app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
@@ -62,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //リストクリック処理
-
             }
         });
     }
