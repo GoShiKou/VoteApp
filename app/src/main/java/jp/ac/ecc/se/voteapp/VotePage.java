7df6050bb8d2da09ca8945e00b410c8debe182e9
森本慎吾 <mossan513@gmail.com>
2024-01-11 11:45:53 +0900

    ai

--- app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class VotePage extends AppCompatActivity {
 
@@ -21,7 +21,22 @@ public class VotePage extends AppCompatActivity {
         TextView title = findViewById(R.id.taitoru);
         Button koment = findViewById(R.id.Comment);
         Button back = findViewById(R.id.back);
+        GridView glisuto = findViewById(R.id.Glist);
+        Intent intent = new Intent(this, Comment.class);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        koment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                startActivity(intent);
 
-    }}
+            }
+        });
+    }
+}
