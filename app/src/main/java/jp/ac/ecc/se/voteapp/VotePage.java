c2016f8ac774957532585b60e2b977898a426a79
森本慎吾 <mossan513@gmail.com>
2024-01-12 10:11:36 +0900

    ai

--- app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
@@ -1,10 +1,13 @@
 package jp.ac.ecc.se.voteapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,31 +15,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class VotePage extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votepage);
-
-
         TextView title = findViewById(R.id.taitoru);
         Button koment = findViewById(R.id.Comment);
         Button back = findViewById(R.id.back);
-        GridView glisuto = findViewById(R.id.Glist);
+        ListView sentaku = findViewById(R.id.sentakusi);
+        ImageView gazou = findViewById(R.id.gazou);
+
         Intent intent = new Intent(this, Comment.class);
 
+
+        //前のページに戻る
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        //コメントページ
         koment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(intent);
-
             }
         });
+
+        //titleを持ってくる
+
+
+
+
+
+
     }
 }
