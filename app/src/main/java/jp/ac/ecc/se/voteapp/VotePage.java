723f9cd2ac4dee0a262d3ddbf768994665511046
森本慎吾 <mossan513@gmail.com>
2024-01-12 11:12:20 +0900

    ai

--- app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/VotePage.java
@@ -2,6 +2,7 @@ package jp.ac.ecc.se.voteapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.hardware.biometrics.BiometricManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,24 +10,48 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.net.Uri;
+import java.util.ArrayList;
+import android.content.Intent;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
 public class VotePage extends AppCompatActivity {
+    TextView showTitle;
+    ImageView showImage;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    Uri imageUri;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votepage);
-        TextView title = findViewById(R.id.taitoru);
+
         Button koment = findViewById(R.id.Comment);
         Button back = findViewById(R.id.back);
         ListView sentaku = findViewById(R.id.sentakusi);
         ImageView gazou = findViewById(R.id.gazou);
 
-        Intent intent = new Intent(this, Comment.class);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        Intent intentmain = new Intent(this, MainActivity.class);
+        Intent intent = getIntent();
+
+        showTitle = findViewById(R.id.taitoru);
+        String title = intent.getStringExtra("title");
+        showTitle.setText(title);
+
+
+//        Intent intent = new Intent(this, Comment.class);
 
 
         //前のページに戻る
@@ -47,9 +72,30 @@ public class VotePage extends AppCompatActivity {
         //titleを持ってくる
 
 
+    }
 
+    private void removeTodoItem(SharedPreferences pref, SharedPreferences.Editor editor, int position) {
+        String str_titles = pref.getString("title", "");
+        String[] list = str_titles.split(",");
+        ArrayList<String> titleList = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            titleList.add(list[i]);
 
+        }
+        if (position < titleList.size()) {
+            editor.remove("title");
 
+            editor.putString("title", arrayToString(titleList));
 
+        }
+    }
+
+    private String arrayToString(ArrayList<String> title) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : title) {
+            sb.append(s).append(",");
+
+        }
+        return sb.toString().replaceAll("$", "");
     }
 }
