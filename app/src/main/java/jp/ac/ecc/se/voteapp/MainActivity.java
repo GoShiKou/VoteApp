18559126a1f4d34a4e1b5d0e2cae4d6b1aea7a67
NGUYENTHISUONG <suongnguyen182001@gmail.com>
2024-01-26 10:40:08 +0900

    commit

--- app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
+++ app/src/main/java/jp/ac/ecc/se/voteapp/MainActivity.java
@@ -80,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
         searchText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 adapter.getFilter().filter(s);
